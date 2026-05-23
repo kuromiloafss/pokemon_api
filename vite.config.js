26048446f9e12423027b0e8bd94a4697c7374cd4
Seila Salsabiela <seila.salsabiela@gmail.com>
2026-05-23 16:31:19 +0700

    fix bug vercel3

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    // Memaksa Vite pakai esbuild standar (bukan lightningcss yang rewel cache)
+    transformer: 'esbuild',
+    minify: 'esbuild'
+  }
 })
